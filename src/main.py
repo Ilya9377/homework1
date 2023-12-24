3597d50e5a4eab6146607bc06783bec8d5b6f062
Ilya Fomin <7foma7@mail.ru>
2023-12-24 19:34:25 +0300

    add tests

--- src/main.py
+++ src/main.py
@@ -1,47 +1,107 @@
 class Category(object):
+    """Класс Category предназначен для хранения информации о категории товаров.
+    Каждый экземпляр класса содержит название категории, описание и список продуктов.
+
+    Атрибуты:
+        _name (str): Название категории.
+        _description (str): Описание категории.
+        _product (list): Список продуктов в данной категории.
+        _unic (int): Классовая переменная, отслеживающая количество созданных экземпляров.
+        _count_products (int): Количество продуктов в категории."""
     _unic = 0
 
-    def __init__(self, name: str, description: str):
+    def __init__(self, name: str, description: str, product_list=None):
+        """Инициализирует экземпляр категории с заданным названием, описанием и списком продуктов."""
+        if product_list is None:
+            product_list = []
         self._name = name
         self._description = description
-        self._product = []
+        self._product = product_list
         Category._unic += 1
         self._count_products = 0
 
     def add(self, product):
+        """Добавляет объект product в список продуктов категории."""
         self._product.append(product)
 
     def products(self):
+        """Выводит информацию о всех продуктах в категории и возвращает список продуктов."""
+        for product in self._product:
+            print(f"{product.name}, {product.price} руб. Остаток: {product.amount} шт.")
         return self._product
 
+    @property
     def name(self):
+        """Возвращает название категории."""
         return self._name
 
+    @property
     def description(self):
+        """Возвращает описание категории."""
         return self._description
 
 
 class Product(object):
+    """
+    Класс Product предназначен для создания и хранения данных о товаре.
+
+    Атрибуты:
+        _name (str): Название продукта.
+        _description (str): Описание продукта.
+        _price (float): Цена продукта.
+        _amount (int): Количество единиц продукта.
+        _unic (int): Классовая переменная, отслеживающая количество созданных экземпляров.
+        """
     _unic = 0
 
     def __init__(self, name: str, description: str, price: float, amount: int):
+        """Инициализирует экземпляр продукта с заданными характеристиками."""
         self._name = name
         self._description = description
         self._price = price
         self._amount = amount
         Product._unic += 1
 
-    def my_unic(self):
-        return Product._unic
+    @staticmethod
+    def create_product(name: str, description: str, price: float, amount: int):
+        """Создает и возвращает экземпляр класса Product."""
+        return Product(name, description, price, amount)
 
-    def name(self):
-        return self._name
-
-    def description(self):
-        return self._description
-
+    @property
     def price(self):
+        """Возвращает цену продукта."""
         return self._price
 
+    @price.setter
+    def price(self, new_price):
+        """
+        Устанавливает новую цену продукта.
+
+        Если новая цена ниже текущей, запрашивает подтверждение для изменения цены.
+        """
+        if self._price > new_price:
+            answer = input("Ввёная цена ниже текущей. Подтвердить? (y/n)")
+            if answer == "y":
+                self._price = new_price
+            else:
+                print("Действие отменено")
+
+    @price.deleter
+    def price(self):
+        """Удаляет информацию о цене продукта."""
+        del self._price
+
+    @property
+    def name(self):
+        """Возвращает название продукта."""
+        return self._name
+
+    @property
+    def description(self):
+        """Возвращает описание продукта."""
+        return self._description
+
+    @property
     def amount(self):
+        """Возвращает количество единиц продукта на складе."""
         return self._amount
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,49 @@
-from src.main import *
+import pytest
+
+from src.main import Product, Category
 
 
-def fuel_filling():
-    prod = Product("appel", "green", 100, 20)
-    assert prod.name() == "appel"
-    assert prod.description() == "green"
-    assert prod.price() == 100
-    assert prod.amount() == 20
+@pytest.fixture
+def check_product_data():
+    """Фикстура pytest для предоставления тестовых данных продукта."""
+    return ["appel", "green", 100, 20]
 
 
-def test_category():
-    categ = Category("appel", "green")
-    prod = Product("appel", "green", 100, 20)
+def test_product_initialization(check_product_data):
+    """
+    Тестирование создания экземпляра класса Product.
+    Используя фикстуру check_product_data, тест проверяет,
+    что все атрибуты продукта инициализированы корректно.
+    """
+    product_data = check_product_data
+    prod = Product(*product_data)
+    assert prod.name == product_data[0]
+    assert prod.description == product_data[1]
+    assert prod.price == product_data[2]
+    assert prod.amount == product_data[3]
+
+
+def test_category_initialization_and_add_product():
+    """
+    Тестирование инициализации категории и добавления продукта в категорию.
+
+    Создает экземпляр Category и экземпляр Product, затем добавляет продукт в категорию.
+    Проверяет корректность названия и описания категории, корректное отображение
+    уникального идентификатора и список продуктов после добавления в категорию.
+    """
+    category_name = "fruits"
+    category_description = "sweet and sour"
+    product_name = "apple"
+    product_description = "green apple"
+    product_price = 100
+    product_amount = 20
+    # Инициализация категории
+    categ = Category(category_name, category_description)
+    # Создание и добавление продукта в категорию
+    prod = Product(product_name, product_description, product_price, product_amount)
     categ.add(prod)
-    assert categ.name() == "appel"
-    assert categ.description() == "green"
+    # Проверки
+    assert categ.name == category_name
+    assert categ.description == category_description
     assert categ._unic == 1
-    assert categ.products() == [prod]
+    assert prod in categ.products()
